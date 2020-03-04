bc1042f1353b9f0835e112db284d54877b32deee
LHGSSH <liamhg1@yahoo.com>
2020-03-04 14:25:44 -0600

    Improved design of home page.

---
+++ css/js/app.js
@@ -0,0 +1,5 @@
+/**
+ * 1. Have a page that calls the Google Maps API. User should type in zip code, pin should appear on
+ * map, and page should say "sorry, no carpools available"
+ * 2. Have a faux sign-up sheet where users can sign up for the service.
+ */
