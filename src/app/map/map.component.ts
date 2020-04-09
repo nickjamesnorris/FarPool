3344275b9ada0eb675d17d7e021af374b373350b
Nick Norris <43795207+100eyez@users.noreply.github.com>
2020-04-09 12:52:34 -0500

    Map gets user location

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,16 +1,51 @@
-import { Component, OnInit } from '@angular/core';
-import { GoogleMapsModule } from '@angular/google-maps'
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { GoogleMap, MapInfoWindow, MapMarker } from "@angular/google-maps";
 
 @Component({
-  selector: 'app-map',
-  templateUrl: './map.component.html',
-  styleUrls: ['./map.component.css']
+  selector: "app-map",
+  templateUrl: "./map.component.html",
+  styleUrls: ["./map.component.css"]
 })
+
 export class MapComponent implements OnInit {
+  
+  @ViewChild(GoogleMap, { static: false }) map: GoogleMap;
+  @ViewChild(MapInfoWindow, { static: false }) info: MapInfoWindow;
 
-  constructor() { }
+  zoom = 18;
+  center: google.maps.LatLngLiteral;
+  options: google.maps.MapOptions = {
+    zoomControl: true,
+    scrollwheel: false,
+    disableDoubleClickZoom: true,
+    mapTypeId: "hybrid"
+  };
+  markers = [];
+  infoContent = "";
 
-  ngOnInit(): void {
+  ngOnInit() {
+    navigator.geolocation.getCurrentPosition(x => {
+      this.center = {
+        lat: x.coords.latitude,
+        lng: x.coords.longitude
+      };
+      this.markers.push({
+        position: {
+          lat: x.coords.latitude,
+          lng: x.coords.longitude
+        },
+        label: {
+          color: "white",
+          border: "black",
+          text: "Marker Label"
+        },
+        title: "Marker Title",
+        info: "Marker info",
+        options: {
+          animation: google.maps.Animation.DROP
+        }
+      });
+    });
   }
-
+  
 }
