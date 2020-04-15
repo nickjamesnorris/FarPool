1f482f67a3a7e4979b0e86236410d2bef918b293
LHGSSH <liamhg1@yahoo.com>
2020-04-15 16:11:54 -0500

    Fixed some styling in create carpool form.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { HomepageComponent } from './homepage/homepage.component';
 import { AboutComponent } from './about/about.component';
+import { CreateACarpoolComponent } from './create-a-carpool/create-a-carpool.component';
 
 
 const routes: Routes = [
   { path: '', component: HomepageComponent },
-  { path: 'about', component: AboutComponent }
+  { path: 'about', component: AboutComponent },
+  { path: 'createcarpool', component: CreateACarpoolComponent}
 ];
 
 @NgModule({
