5a86f7bdbec464e06dba9c85c42914b1b8aa8c3e
LHGSSH <liamhg1@yahoo.com>
2020-04-24 21:08:16 -0500

    Fixed problem with authentication.

--- src/app/create-a-carpool/create-a-carpool.component.ts
+++ src/app/create-a-carpool/create-a-carpool.component.ts
@@ -10,7 +10,7 @@ import { Subscription } from 'rxjs';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { MapsAPILoader } from '@agm/core';
-import {} from 'googlemaps';
+import { } from 'googlemaps';
 
 @Component({
   selector: 'app-create-a-carpool',
@@ -29,27 +29,30 @@ export class CreateACarpoolComponent implements OnInit {
 
   constructor(private fb: FormBuilder, private flashMessagesService: FlashMessagesService, private router: Router, private carpoolService: CarpoolService, public userService: UserService, private afAuth: AngularFireAuth, private afs: AngularFirestore, private mapsAPILoader: MapsAPILoader, private cdRef: ChangeDetectorRef) {
     this.createForm();
-   }
+  }
 
   ngOnInit(): void {
     this.userService.user$.subscribe((user) => {
-      this.hidden = false; //Show the form
-      let startingPoint = document.getElementById('startingPoint') as HTMLInputElement;
-      let destination = document.getElementById('destination') as HTMLInputElement;
+      if (user != null) {
+        console.log(user);
+        this.hidden = false; //Show the form
+        let startingPoint = document.getElementById('startingPoint') as HTMLInputElement;
+        let destination = document.getElementById('destination') as HTMLInputElement;
 
-      this.mapsAPILoader.load().then(() => {
-        let startingPointAutocomplete = new google.maps.places.Autocomplete(startingPoint);
-        let destinationAutocomplete = new google.maps.places.Autocomplete(destination);
+        this.mapsAPILoader.load().then(() => {
+          let startingPointAutocomplete = new google.maps.places.Autocomplete(startingPoint);
+          let destinationAutocomplete = new google.maps.places.Autocomplete(destination);
 
-        startingPointAutocomplete.addListener("place_changed", () => {
-          let place = startingPointAutocomplete.getPlace();
-          this.carpoolForm.get('startingPoint').setValue(place.formatted_address);
+          startingPointAutocomplete.addListener("place_changed", () => {
+            let place = startingPointAutocomplete.getPlace();
+            this.carpoolForm.get('startingPoint').setValue(place.formatted_address);
+          });
+          destinationAutocomplete.addListener("place_changed", () => {
+            let place = destinationAutocomplete.getPlace();
+            this.carpoolForm.get('destination').setValue(place.formatted_address);
+          });
         });
-        destinationAutocomplete.addListener("place_changed", () => {
-          let place = destinationAutocomplete.getPlace();
-          this.carpoolForm.get('destination').setValue(place.formatted_address);
-        });
-      });
+      }
     });
   }
 
@@ -69,8 +72,8 @@ export class CreateACarpoolComponent implements OnInit {
   }
 
   async onSubmit() {
-    console.log(this.carpoolForm.get('destination').value);
-    
+    console.log("submission");
+
     let currentUser = await this.userService.getCurrentUser();
     let newCarpool = new Carpool({
       carpoolName: this.carpoolForm.get('carpoolName').value,
